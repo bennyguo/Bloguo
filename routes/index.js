071033e814065ba7b23445888bf9c30952bb909b
bennyguo <bennyguo@163.com>
2017-02-02 21:31:25 +0800

    Simple views of index page and article page.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/article', function(req, res, next) {
+  res.render('article', { title: 'Express' });
+});
+
 module.exports = router;
