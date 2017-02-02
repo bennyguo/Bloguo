90276294d2bb0e6f1ae5e87a57bdef498ea8cc3e
bennyguo <bennyguo@163.com>
2017-02-03 00:22:12 +0800

    Successfully test the combination of RESTful and Vue.js.

--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,66 @@ router.get('/article', function(req, res, next) {
   res.render('article', { title: 'Express' });
 });
 
+var tmpdata1 = {
+					title: 'My First Article',
+					url: 'https://www.baidu.com',
+					year: 2017,
+					month: 1,
+					day: 15,
+					hour: 13,
+					minute: 45,
+					second: 30,
+					nvisits: 1367,
+					ncomments: 15,
+					tag: 'javascript',
+					abstract: 'This is my new home! Welcome to visit!'
+				};
+
+var tmpdata2 = {
+					title: 'My Second Article',
+					url: 'https://www.baidu.com',
+					year: 2017,
+					month: 1,
+					day: 13,
+					hour: 11,
+					minute: 11,
+					second: 02,
+					nvisits: 211,
+					ncomments: 1,
+					tag: 'javascript',
+					abstract: 'This is my new home! Welcome to visit!'
+				};
+
+var tmparticle = {
+					title: 'My First Article',
+					url: 'https://www.baidu.com',
+					year: 2017,
+					month: 1,
+					day: 15,
+					hour: 13,
+					minute: 45,
+					second: 30,
+					nvisits: 1367,
+					ncomments: 15,
+					tag: 'javascript',
+					text: 'This is my new home! Welcome to visit!'
+				};
+
+router.get('/articles', function(req, res, next) {
+	res.json({
+		status: 'ok',
+		dataset: [
+			tmpdata1,
+			tmpdata2
+		]
+	})
+})
+
+router.get('/article/test', function(req, res, next) {
+	res.json({
+		status: 'ok',
+		data: tmparticle
+	})
+})
+
 module.exports = router;
